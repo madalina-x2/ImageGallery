7a7f6d7af0837363b6968bf39e5d7c5bb0f7511f
Madalina Sinca <madalinasinca@Madalinas-MacBook-Pro.local>
2019-09-17 15:10:35 +0300

    added image gallery storyboard items

---
+++ Image-Gallery-Assignment5/Image-Gallery-Assignment5/Controllers/GallerySelectionTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GallerySelectionTableViewController.swift
+//  Image-Gallery-Assignment5
+//
+//  Created by Madalina Sinca on 17/09/2019.
+//  Copyright © 2019 Madalina Sinca. All rights reserved.
+//
+
+import UIKit
+
+class GallerySelectionTableViewController: UITableViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
