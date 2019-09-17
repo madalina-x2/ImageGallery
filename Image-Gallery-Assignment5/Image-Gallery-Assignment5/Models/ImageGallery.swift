8f89fdcb4e89319294709dbb768f415f1786759b
Madalina Sinca <madalinasinca@Madalinas-MacBook-Pro.local>
2019-09-17 14:43:58 +0300

    added models' skeletons

---
+++ Image-Gallery-Assignment5/Image-Gallery-Assignment5/Models/ImageGallery.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGallery.swift
+//  Image-Gallery-Assignment5
+//
+//  Created by Madalina Sinca on 17/09/2019.
+//  Copyright © 2019 Madalina Sinca. All rights reserved.
+//
+
+import Foundation
+
+struct ImageGallery {
+    
+    // MARK: - Properties
+    
+    var images: [Image]
+    var title: String
+}
