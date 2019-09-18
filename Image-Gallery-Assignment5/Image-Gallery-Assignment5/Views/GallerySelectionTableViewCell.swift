1a6355e6455a3d2a179f97746cfa02193f2cf448
Madalina Sinca <madalinasinca@Madalinas-MacBook-Pro.local>
2019-09-18 17:31:53 +0300

    implemented textfield methods via UITextFieldDelegate interface methods

--- Image-Gallery-Assignment5/Image-Gallery-Assignment5/Views/GallerySelectionTableViewCell.swift
+++ Image-Gallery-Assignment5/Image-Gallery-Assignment5/Views/GallerySelectionTableViewCell.swift
@@ -8,17 +8,61 @@
 
 import UIKit
 
-class GallerySelectionTableViewCell: UITableViewCell {
+class GallerySelectionTableViewCell: UITableViewCell, UITextFieldDelegate {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // MARK: - Properties
+    
+    internal var delegate: GallerySelectionTableViewCellDelegate?
+    
+    @IBOutlet weak var galleryTitleTextField: UITextField!
+    
+    var title: String {
+        set {
+            galleryTitleTextField?.text = newValue
+        }
+        get {
+            return galleryTitleTextField.text ?? ""
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override internal var isEditing: Bool {
+        didSet {
+            galleryTitleTextField.isEnabled = isEditing
+            
+            if isEditing == true {
+                galleryTitleTextField.becomeFirstResponder()
+            } else {
+                galleryTitleTextField.resignFirstResponder()
+            }
+        }
     }
+    
+    // MARK: - UITextFieldDelegate Methods
+    
+    @objc internal func titleDidChange(_ sender: UITextField) {
+        guard let title = sender.text, title != "" else {
+            return
+        }
+        
+        delegate?.titleDidChange(sender.text ?? "", in: self)
+    }
+    
+    override internal var canBecomeFirstResponder: Bool {
+        return isEditing
+    }
+    
+    internal func textFieldDidEndEditing(_ textField: UITextField) {
+        isEditing = false
+    }
+    
+    internal func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        isEditing = false
+        return true
+    }
+}
 
+// MARK: - Protocols
+
+protocol GallerySelectionTableViewCellDelegate {
+    func titleDidChange(_ title: String, in cell: UITableViewCell)
 }
