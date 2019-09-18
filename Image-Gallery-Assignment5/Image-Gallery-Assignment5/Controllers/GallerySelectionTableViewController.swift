e95e547e3bdea01bf0b75182f4fc6a0b5261a494
Madalina Sinca <madalinasinca@Madalinas-MacBook-Pro.local>
2019-09-18 16:30:19 +0300

    added gallery name text field

--- Image-Gallery-Assignment5/Image-Gallery-Assignment5/Controllers/GallerySelectionTableViewController.swift
+++ Image-Gallery-Assignment5/Image-Gallery-Assignment5/Controllers/GallerySelectionTableViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class GallerySelectionTableViewController: UITableViewController {
 
+    // MARK: - Read-Only Properties
+    
+    private(set) var availableGalleries: [ImageGallery] = []
+    private(set) var recentlyDeletedGalleries: [ImageGallery] = []
+    
+    // MARK: - Private Properties
+    
+    private var detailController: GalleryDisplayCollectionViewController? {
+        return splitViewController?.viewControllers.last?.contents as? GalleryDisplayCollectionViewController
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
---
+++ Image-Gallery-Assignment5/Image-Gallery-Assignment5/Views/GallerySelectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GallerySelectionTableViewCell.swift
+//  Image-Gallery-Assignment5
+//
+//  Created by Madalina Sinca on 18/09/2019.
+//  Copyright © 2019 Madalina Sinca. All rights reserved.
+//
+
+import UIKit
+
+class GallerySelectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
